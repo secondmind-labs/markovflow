48737a8feb04bac48932cb8a6db3dd9eda36dd50
Prakhar Verma <prakhar.verma7@gmail.com>
2022-01-11 17:00:02 +0530

    Adding SDEs and basic functionalities (#12)
    * Adding SDEs and basic functionalities
    
    * SDE script docstring modified
    
    * Euler-Maruyama with tf scan, batches included; other minor
    
    * Added test for Euler-Maruyama simulated values
    
    * converting linearize SDE to a method; other cleanup
    
    * Batching in linearize SDE; tests modified
    
    * minor docstring correction in SSM
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update tests/unit/test_sde.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * Update markovflow/sde/sde_utils.py
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>
    
    * incorporating suggestions and other reorganization
    
    * Minor
    
    * dtype changes
    
    Co-authored-by: Vincent Adam <38107974+thevincentadam@users.noreply.github.com>

---
+++ markovflow/sde/sde_utils.py
@@ -0,0 +1,129 @@
+#
+# Copyright (c) 2021 The Markovflow Contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Utility functions for SDE"""
+
+import tensorflow as tf
+
+from gpflow.base import TensorType
+
+from markovflow.sde import SDE
+from markovflow.state_space_model import StateSpaceModel
+
+
+def euler_maruyama(sde: SDE, x0: tf.Tensor, time_grid: tf.Tensor) -> tf.Tensor:
+    """
+    Numerical Simulation of SDEs of type: dx(t) = f(x,t)dt + L(x,t)dB(t) using the Euler-Maruyama Method.
+
+    ..math:: x(t+1) = x(t) + f(x,t)dt + L(x,t)*sqrt(dt*q)*N(0,I)
+
+    :param sde: Object of SDE class
+    :param x0: state at start time, t0, with shape (n_batch, state_dim)
+    :param time_grid: A homogeneous time grid for simulation, (num_transitions, )
+
+    :return: Simulated SDE values, (n_batch, num_transitions+1, state_dim)
+
+    Note: evaluation time grid is [t0, tn], x0 value is appended for t0 time.
+    Thus, simulated values are (num_transitions+1).
+    """
+
+    DTYPE = x0.dtype
+    num_data = time_grid.shape[0]
+    state_dim = x0.shape[-1]
+    n_batch = x0.shape[0]
+
+    f = sde.drift
+    l = sde.diffusion
+
+    def _step(current_val, nxt_val):
+        x, t = current_val
+        _, t_nxt = nxt_val
+        dt = t_nxt[0] - t[0]  # As time grid is homogeneous
+        diffusion_term = tf.cast(l(x, t) * tf.math.sqrt(dt), dtype=DTYPE)
+        x_nxt = x + f(x, t) * dt + tf.random.normal(x.shape, dtype=DTYPE) @ diffusion_term
+        return x_nxt, t_nxt
+
+    # [num_data, batch_shape, state_dim] for tf.scan
+    sde_values = tf.zeros((num_data, n_batch, state_dim), dtype=DTYPE)
+
+    # Adding time for batches for tf.scan
+    t0 = tf.zeros((n_batch, 1), dtype=DTYPE)
+    time_grid = tf.reshape(time_grid, (-1, 1, 1))
+    time_grid = tf.repeat(time_grid, n_batch, axis=1)
+
+    sde_values, _ = tf.scan(_step, (sde_values, time_grid), (x0, t0))
+
+    # [batch_shape, num_data, state_dim]
+    sde_values = tf.transpose(sde_values, [1, 0, 2])
+
+    # Appending the initial value
+    sde_values = tf.concat([tf.expand_dims(x0, axis=1), sde_values], axis=1)
+
+    return sde_values
+
+
+def linearize_sde(
+    sde: SDE,
+    transition_times: TensorType,
+    q_mean: TensorType,
+    q_covar: TensorType,
+    initial_mean: TensorType,
+    initial_chol_covariance: TensorType,
+    process_chol_covariances: TensorType,
+) -> StateSpaceModel:
+    """
+    Linearizes the SDE (with fixed diffusion) on the basis of the Gaussian over states
+
+    Note: this currently only works for sde with a state dimension of 1.
+
+    ..math:: q(\cdot) \sim N(q_{mean}, q_{covar})
+
+    ..math:: A_{i}^{*} = E_{q(.)}[d f(x)/ dx]
+    ..math:: b_{i}^{*} = E_{q(.)}[f(x)] - A_{i}^{*}  E_{q(.)}[x]
+
+    :param sde: SDE to be linearized.
+    :param transition_times: Transition_times, (num_transitions, )
+    :param q_mean: mean of Gaussian over states with shape (n_batch, num_states, state_dim).
+    :param q_covar: covariance of Gaussian over states with shape (n_batch, num_states, state_dim, state_dim).
+    :param initial_mean: The initial mean, with shape ``[n_batch, state_dim]``.
+    :param initial_chol_covariance: Cholesky of the initial covariance, with shape ``[n_batch, state_dim, state_dim]``.
+    :param process_chol_covariances: Cholesky of the noise covariance matrices, with shape
+        ``[n_batch, num_states, state_dim, state_dim]``.
+
+    :return: the state-space model of the linearized SDE.
+    """
+
+    assert sde.state_dim == 1
+    E_f = sde.E_drift(q_mean, q_covar)
+    E_x = q_mean
+
+    A = sde.E_gradient_drift(q_mean, q_covar)
+    b = E_f - A * E_x
+    A = tf.linalg.diag(A)
+
+    transition_deltas = tf.reshape(transition_times[1:] - transition_times[:-1], (1, -1, 1))
+    state_transitions = A * tf.expand_dims(transition_deltas, -1)
+    state_offsets = b * transition_deltas
+    chol_process_covariances = process_chol_covariances * tf.expand_dims(
+        tf.sqrt(transition_deltas), axis=-1
+    )
+
+    return StateSpaceModel(
+        initial_mean=initial_mean,
+        chol_initial_covariance=initial_chol_covariance,
+        state_transitions=state_transitions,
+        state_offsets=state_offsets,
+        chol_process_covariances=chol_process_covariances,
+    )
---
+++ tests/unit/test_sde.py
@@ -0,0 +1,119 @@
+#
+# Copyright (c) 2021 The Markovflow Contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Module containing the unit tests for the SDE related functions and classes"""
+import pytest
+
+import tensorflow as tf
+import numpy as np
+import gpflow
+
+from markovflow.sde.sde_utils import euler_maruyama, linearize_sde
+from markovflow.sde import OrnsteinUhlenbeckSDE
+
+tf.random.set_seed(33)
+
+DTYPE = tf.float32
+gpflow.config.set_default_float(DTYPE)
+
+
+@pytest.fixture(name="setup")
+def _setup(state_dim, batch_shape):
+    """"""
+    t0 = 0.
+    t1 = 1.
+    n_transitions = 10
+    n_batch = batch_shape.dims[0].value if batch_shape.ndims > 0 else 1
+    x0_shape = (n_batch, state_dim)
+
+    dt = float((t1 - t0) / n_transitions)
+    x0 = tf.random.normal(x0_shape, dtype=DTYPE)
+
+    decay = tf.random.normal((1, 1), dtype=DTYPE)
+    q = tf.eye(state_dim, dtype=DTYPE)
+    ou_sde = OrnsteinUhlenbeckSDE(decay, q)
+
+    time_grid = tf.cast(tf.linspace(t0 + dt, t1, n_transitions), dtype=DTYPE)
+
+    return state_dim, decay, ou_sde, x0, dt, time_grid
+
+
+def test_linearize_sde_ou_statedim_1(setup):
+    """
+    Test for checking the linearize sde method for Ornstein-Uhlenbeck SDE with state_dim=1.
+    """
+
+    state_dim, decay, ou_sde, x0, dt, time_grid = setup
+
+    # As currently only 1 state dim is supported
+    if state_dim != 1:
+        return True
+
+    q_mean = euler_maruyama(ou_sde, x0, time_grid)
+    q_mean = tf.convert_to_tensor(q_mean, dtype=DTYPE)
+    q_mean = q_mean[:, :-1, :]  # [n_batch, n_transitions, state_dim]
+
+    q_covar = tf.zeros((q_mean.shape + state_dim), dtype=DTYPE)
+    covar_diag = 1e-4 * tf.ones_like(q_mean)
+    q_covar = tf.linalg.set_diag(q_covar, covar_diag)  # [n_batch, n_transitions, state_dim, state_dim]
+
+    x0_covar_chol = tf.linalg.cholesky(q_covar[:, 0, :, :])  # [n_batch, state_dim, state_dim]
+    noise_covar = 1e-2 * tf.ones_like(q_covar)  # [n_batch, n_transitions, state_dim, state_dim]
+
+    # Adding t0 to time_grid
+    time_grid = tf.concat([tf.zeros((1,), dtype=DTYPE), time_grid], axis=0)
+
+    linearized_ssm = linearize_sde(ou_sde, time_grid, q_mean, q_covar, x0, x0_covar_chol, noise_covar)
+
+    expected_A = tf.zeros_like(q_covar)  # [n_batch, n_transitions, state_dim, state_dim]
+    expected_b = tf.zeros_like(q_mean) * dt  # [n_batch, n_transitions, state_dim]
+    expected_A = tf.linalg.set_diag(expected_A, -decay + expected_b) * dt
+
+    np.testing.assert_allclose(linearized_ssm.state_transitions, expected_A, atol=1e-3)
+    np.testing.assert_allclose(linearized_ssm.state_offsets, expected_b, atol=1e-3)
+
+
+def test_euler_maruyama_shapes(setup):
+    """Test checking the shape of the simulated values by Euler-Maruyama for Ornstein-Uhlenbeck SDE."""
+    state_dim, decay, ou_sde, x0, dt, t = setup
+    n_transitions = t.shape[0]
+
+    simulated_values = euler_maruyama(ou_sde, x0, t)
+
+    assert simulated_values.shape[0] == x0.shape[0]
+    assert simulated_values.shape[1] == n_transitions+1
+    assert simulated_values.shape[-1] == state_dim
+
+
+def test_euler_maruyama_value(setup):
+    """
+    Test checking the simulated values by Euler-Maruyama for Ornstein-Uhlenbeck SDE.
+    We make the diffusion term zero i.e. there is no noise and the drift value is set as 1.
+    """
+    state_dim, decay, ou_sde, x0, dt, t = setup
+    n_transitions = t.shape[0]
+    n_batch = x0.shape[0]
+
+    # make diffusion zero and simulate
+    ou_sde.q = 1e-20 * ou_sde.q
+    simulated_values = euler_maruyama(ou_sde, x0, t)
+
+    expected_values = tf.zeros((n_transitions, n_batch, state_dim), dtype=DTYPE)
+    expected_values = tf.concat([tf.expand_dims(x0, axis=0), expected_values], axis=0)
+
+    expected_values = tf.scan(lambda a, x: a + (-decay*a*dt), expected_values)
+    expected_values = tf.transpose(expected_values, [1, 0, 2])
+
+    np.testing.assert_allclose(simulated_values, expected_values, atol=1e-6)
