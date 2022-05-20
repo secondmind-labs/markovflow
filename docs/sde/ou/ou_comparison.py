stop gradient fixed; CVI-GPR included in OU

docs/sde/ou/ou_comparison.py
@@ -9,7 +9,8 @@ from markovflow.sde.sde import OrnsteinUhlenbeckSDE, PriorOUSDE
 from markovflow.kernels import OrnsteinUhlenbeck
 from markovflow.models.cvi_sde import SDESSM
 from markovflow.models.vi_sde import VariationalMarkovGP
-from ou_utils import generate_data, get_gpr, predict_vgp, predict_ssm, predict_gpr, plot_observations, plot_posterior
+from ou_utils import generate_data, get_gpr, predict_vgp, predict_ssm, predict_gpr, plot_observations, plot_posterior, \
+    get_cvi_gpr, predict_cvi_gpr
 
 tf.random.set_seed(83)
 np.random.seed(83)
@@ -24,9 +25,9 @@ q = 0.5
 noise_var = 0.05
 x0 = 1.
 
-t0, t1 = 0.0, 10.
+t0, t1 = 0.0, 5.
 simulation_dt = 0.01  # Used for Euler-Maruyama
-n_observations = 50
+n_observations = 30
 
 learn_prior_sde = True
 prior_initial_decay_val = tf.random.normal((1, 1), dtype=DTYPE)  # Used when learning prior sde
@@ -59,15 +60,20 @@ input_data = (observation_grid, tf.constant(tf.squeeze(observation_vals, axis=0)
 """
 GPR
 """
+likelihood_gpr = Gaussian(noise_stddev**2)
+
 if learn_prior_sde:
     kernel = OrnsteinUhlenbeck(decay=prior_initial_decay_val.numpy().item(), diffusion=q)
 else:
     kernel = OrnsteinUhlenbeck(decay=decay, diffusion=q)
-gpr_model = get_gpr(input_data, kernel, train=learn_prior_sde, noise_stddev=noise_stddev)
 
+cvi_gpr_model = get_cvi_gpr(input_data, kernel, likelihood_gpr, train=learn_prior_sde)
+
+gpr_model = get_gpr(input_data, kernel, train=learn_prior_sde, noise_stddev=noise_stddev)
 gpr_log_likelihood = gpr_model.log_likelihood().numpy()
 print(f"GPR Likelihood : {gpr_log_likelihood}")
 
+print(f"CVI-GPR ELBO: {cvi_gpr_model.classic_elbo()}")
 """
 SDE-SSM
 """
@@ -90,6 +96,7 @@ ssm_model = SDESSM(input_data=input_data, prior_sde=prior_sde_ssm, grid=time_gri
 
 # For OU we know this relation for variance
 ssm_model.initial_chol_cov = tf.linalg.cholesky((q/(2 * decay)) * tf.ones_like(ssm_model.initial_chol_cov))
+ssm_model.fx_covs = ssm_model.initial_chol_cov.numpy().item()**2 + 0 * ssm_model.fx_covs
 
 ssm_elbo, ssm_prior_prior_vals = ssm_model.run(update_prior=learn_prior_sde)
 if learn_prior_sde:
@@ -124,13 +131,14 @@ if learn_prior_sde:
 Predict Posterior
 """
 plot_observations(observation_grid.numpy(), observation_vals.numpy())
-m_gpr, s_std_gpr = predict_gpr(gpr_model, time_grid.numpy())
+# m_gpr, s_std_gpr = predict_gpr(gpr_model, time_grid.numpy())  # FOR GPR MODEL
+m_gpr, s_std_gpr = predict_cvi_gpr(cvi_gpr_model, time_grid.numpy(), noise_stddev)
 m_ssm, s_std_ssm = predict_ssm(ssm_model, noise_stddev)
 m_vgp, s_std_vgp = predict_vgp(vgp_model, noise_stddev)
 """
 Compare Posterior
 """
-plot_posterior(m_gpr, s_std_gpr, time_grid.numpy(), "GPR")
+plot_posterior(m_gpr, s_std_gpr, time_grid.numpy(), "CVI-GPR")
 plot_posterior(m_ssm, s_std_ssm, time_grid.numpy(), "SDE-SSM")
 plot_posterior(m_vgp, s_std_vgp, time_grid.numpy(), "VGP")
 plt.legend()
@@ -144,8 +152,8 @@ if learn_prior_sde:
                label="True Value", color="black", linestyles="dashed")
     plt.plot(v_gp_prior_decay_values, label="VGP", color="green")
     plt.plot(ssm_prior_decay_values, label="SDE-SSM", color="blue")
-    plt.hlines(-1 * gpr_model.kernel.decay, 0, max(len(v_gp_prior_decay_values), len(ssm_prior_decay_values)),
-               label="GPR", color="red")
+    plt.hlines(-1 * cvi_gpr_model.kernel.decay, 0, max(len(v_gp_prior_decay_values), len(ssm_prior_decay_values)),
+               label="CVI-GPR", color="red")
     plt.title("Prior Learning")
     plt.legend()
     plt.show()
@@ -178,6 +186,7 @@ if not learn_prior_sde:
 
         ssm_model.prior_sde = OrnsteinUhlenbeckSDE(decay=decay_val, q=true_q)
         ssm_model.initial_chol_cov = tf.linalg.cholesky((q/(2 * decay_val)) * tf.ones_like(ssm_model.initial_chol_cov))
+        ssm_model.fx_covs = ssm_model.initial_chol_cov.numpy().item() ** 2 + 0 * ssm_model.fx_covs
         ssm_elbo_vals.append(ssm_model.classic_elbo())
 
         vgp_model.prior_sde = OrnsteinUhlenbeckSDE(decay=decay_val, q=true_q)

docs/sde/ou/ou_utils.py
@@ -5,10 +5,12 @@ Script for common utility functions needed for OU process experiment.
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from gpflow.likelihoods import Likelihood
 
 from markovflow.models.vi_sde import VariationalMarkovGP
 from markovflow.models.cvi_sde import SDESSM
 from markovflow.models.gaussian_process_regression import GaussianProcessRegression
+from markovflow.models.variational_cvi import CVIGaussianProcess
 from markovflow.kernels import SDEKernel
 from markovflow.sde.sde import OrnsteinUhlenbeckSDE
 from markovflow.sde.sde_utils import euler_maruyama
@@ -67,10 +69,10 @@ def predict_vgp(model: VariationalMarkovGP, noise_stddev: np.ndarray) -> (np.nda
     Predict mean and std-dev for VGP model.
     """
     m, S = model.forward_pass
-    S = (tf.reshape(S, (-1)) + noise_stddev ** 2).numpy()
+    S = tf.reshape(S, (-1)).numpy()
 
     m = m.numpy().reshape(-1)
-    S_std = np.sqrt(S)
+    S_std = np.sqrt(S) + noise_stddev
 
     return m, S_std
 
@@ -81,10 +83,20 @@ def predict_ssm(model: SDESSM, noise_stddev: np.ndarray) -> (np.ndarray, np.ndar
     """
     posterior_ssm = model.posterior_kalman.posterior_state_space_model()
     m, S = posterior_ssm.marginal_means, posterior_ssm.marginal_covariances
-    S = S + noise_stddev ** 2
 
     m = m.numpy().reshape(-1)
-    S_std = np.sqrt(S)
+    S_std = np.sqrt(S) + noise_stddev
+
+    return m, S_std
+
+
+def predict_cvi_gpr(model: CVIGaussianProcess, time_grid: np.ndarray, noise_stddev: np.ndarray) -> [np.ndarray, np.ndarray]:
+    """
+    Predict mean and std-dev for CVI-GPR model.
+    """
+    m, S = model.predict_f(time_grid)
+    m = m.numpy().reshape(-1)
+    S_std = np.sqrt(S) + noise_stddev
 
     return m, S_std
 
@@ -98,6 +110,29 @@ def predict_gpr(model: GaussianProcessRegression, time_grid: np.ndarray) -> [np.
     S_std = np.sqrt(S)
 
     return m, S_std
+
+
+def get_cvi_gpr(input_data: [tf.Tensor, tf.Tensor], kernel: SDEKernel, likelihood: Likelihood,
+                train: bool = False, sites_lr: float = 0.9):
+    cvi_model = CVIGaussianProcess(input_data=input_data, kernel=kernel, likelihood=likelihood,
+                                   learning_rate=sites_lr)
+
+    opt = tf.optimizers.Adam()
+
+    @tf.function
+    def opt_step():
+        opt.minimize(cvi_model.loss, cvi_model.kernel.trainable_variables)
+
+    # FIXME: Remove this hard coding nd do on the basis of ELBO values
+    for _ in range(4):
+        for _ in range(2):
+            cvi_model.update_sites()
+
+        if train:
+            for _ in range(10):
+                opt_step()
+
+    return cvi_model
 
 
 def get_gpr(input_data: [tf.Tensor, tf.Tensor], kernel: SDEKernel, train: bool, noise_stddev: np.ndarray,

markovflow/models/cvi_sde.py
@@ -77,7 +77,7 @@ class SDESSM(CVIGaussianProcess):
         self.state_dim = 1
 
         self._intialize_mean_statistic()
-        self.sites_nat2 = tf.ones_like(self.grid, dtype=self.observations.dtype)[..., None, None] * -1e-10
+        self.sites_nat2 = tf.ones_like(self.grid, dtype=self.observations.dtype)[..., None, None] * -1e-20
 
         self.sites_lr = learning_rate
         self.prior_sde_optimizer = tf.optimizers.SGD(lr=learning_rate)
@@ -222,11 +222,7 @@ class SDESSM(CVIGaussianProcess):
         self.data_sites.nat2.assign(new_nat2)
         self.data_sites.nat1.assign(new_nat1)
 
-        dist_q = self.dist_q
-        fx_mus, fx_covs = dist_q.marginal_means, dist_q.marginal_covariances
-
-        self.fx_mus = fx_mus
-        self.fx_covs = fx_covs
+        self.fx_mus, self.fx_covs = self.dist_q.marginals
 
         return has_converged
 
@@ -333,6 +329,10 @@ class SDESSM(CVIGaussianProcess):
                 A = tf.squeeze(self.dist_p.state_transitions, axis=0)
                 b = tf.squeeze(self.dist_p.state_offsets, axis=0)
 
+                # Stopping gradient
+                A = tf.stop_gradient(A)
+                b = tf.stop_gradient(b)
+
                 # convert from A and b to SDE drift and offset
                 A = (A - tf.eye(self.state_dim, dtype=A.dtype))/(self.grid[1] - self.grid[0])
                 b = b / (self.grid[1] - self.grid[0])
@@ -345,6 +345,7 @@ class SDESSM(CVIGaussianProcess):
                 tmp = tmp * tmp
 
                 sigma = self.prior_sde.q
+                sigma = tf.stop_gradient(sigma)
 
                 val = tmp * (1 / sigma)
 
@@ -398,7 +399,7 @@ class SDESSM(CVIGaussianProcess):
         """
         Run inference and (if required) update prior till convergence.
         """
-        while self.sites_lr > 0.001:
+        while self.sites_lr > 1e-4:
             sites_converged = False
             while not sites_converged:
                 sites_converged = self.update_sites()

markovflow/models/vi_sde.py
@@ -136,7 +136,7 @@ class VariationalMarkovGP:
 
         return ssm.marginal_means, ssm.marginal_covariances
 
-    def E_sde(self, m: tf.Tensor, S: tf.Tensor):
+    def E_sde(self, m: tf.Tensor, S: tf.Tensor, stop_drift_gradient: bool = True):
         """
         E_sde = 0.5 * <(f-f_L)^T \sigma^{-1} (f-f_L)>_{q_t}.
         Apply Gaussian quadrature method to approximate the integral.
@@ -153,10 +153,18 @@ class VariationalMarkovGP:
             b = tf.repeat(self.b, n_pnts, axis=1)
             b = tf.expand_dims(b, axis=-1)
 
-            tmp = self.prior_sde.drift(x=x, t=t) + ((x * A) - b)
+            A = tf.stop_gradient(A)
+            b = tf.stop_gradient(b)
+
+            prior_drift = self.prior_sde.drift(x=x, t=t)
+            if stop_drift_gradient:
+                prior_drift = tf.stop_gradient(prior_drift)
+
+            tmp = prior_drift + ((x * A) - b)
             tmp = tmp * tmp
 
             sigma = self.prior_sde.q
+            sigma = tf.stop_gradient(sigma)
 
             val = tmp * (1 / sigma)
 
@@ -310,7 +318,10 @@ class VariationalMarkovGP:
         """
         KL[q(x0) || p(x0)]
         """
-        dist_qx0 = distributions.Normal(self.q_initial_mean, tf.linalg.cholesky(self.q_initial_cov))
+        q0_mean = tf.stop_gradient(self.q_initial_mean)
+        q0_cov = tf.stop_gradient(self.q_initial_cov)
+
+        dist_qx0 = distributions.Normal(q0_mean, tf.linalg.cholesky(q0_cov))
         dist_px0 = distributions.Normal(self.p_initial_mean, tf.linalg.cholesky(self.p_initial_cov))
         return distributions.kl_divergence(dist_qx0, dist_px0)
 
@@ -339,8 +350,11 @@ class VariationalMarkovGP:
         Function to update the prior SDE.
         """
         m, S = self.forward_pass
+        m = tf.stop_gradient(m)
+        S = tf.stop_gradient(S)
+
         def func():
-            return self.E_sde(m, S) * self.dt + self.KL_initial_state()
+            return self.E_sde(m, S, stop_drift_gradient=False) * self.dt + self.KL_initial_state()
 
         old_val = self.prior_sde.trainable_variables
         self.prior_sde_optimizer.minimize(func, self.prior_sde.trainable_variables)
@@ -368,7 +382,7 @@ class VariationalMarkovGP:
         """
         Run inference till convergence
         """
-        while self.q_lr >= 0.001 and self.x_lr >= 0.001:
+        while self.q_lr >= 1e-4 and self.x_lr >= 1e-4:
             inference_converged = False
             x0_converged = False
             while not inference_converged and not x0_converged: